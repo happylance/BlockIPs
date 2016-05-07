0f4751d166ccc7963e311f71614dd60650d9f92d
Liang Liu <nlprliu@gmail.com>
2016-05-07 10:23:16 -0400

    Only check logs within the previous 4 hours.

--- blockIPs.sh
+++ blockIPs.sh
@@ -4,8 +4,16 @@ set -e
 IPList_file=/etc/apache2/conf/IPList.conf
 check_time=$(date +%Y-%m-%d-%H:%M:%S)
 new_IPs_to_block_file="/tmp/new_IPs_to_block_at_$check_time"
-IPs=$(cat /var/log/apache2/access.log | tail -5000 | grep -v 403 | grep xmlrpc.php | awk '{print $1}' | awk '{count[$1]++}END {for (word in count) if (count[word] > 10) print word}')
-[ -z "$IPs" ] && { echo "$check_time: Done"; exit 0; }
+
+duration_in_hours=4
+let duration=$duration_in_hours*3600
+now=$(date +%s)
+logs=$(tail -5000 /var/log/apache2/access.log)
+recent_log_count=$(echo "$logs" | sed 's/.*\[\([^ ]*\).*/\1/' | sed 's_/_-_g;s/:/ /' | sed 's_\(.*\)_date -d "\1" +%s_' | sh | awk -v now=$now -v duration=$duration '{print now-$1-duration}' | grep "-" | wc -l)
+[ "$recent_log_count" -eq 0 ] && { echo "$check_time: No logs in the previous $duration_in_hours hours. Done"; exit 0; }
+
+IPs=$(echo "$logs" | tail -$recent_log_count | grep -v 403 | grep xmlrpc.php | awk '{print $1}' | awk '{count[$1]++}END {for (word in count) if (count[word] > 10) print word}')
+[ -z "$IPs" ] && { echo "$check_time: No IPs needs to be blocked in recent $recent_log_count logs. Done"; exit 0; }
 
 echo "$IPs" | sed 's/^/Require not ip /' | grep -vxf "$IPList_file" > $new_IPs_to_block_file
 
